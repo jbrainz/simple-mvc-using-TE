f90a096f3091b217b1d126a7b18d101db8ebe559
Pius <jonzyp4reala@gmail.com>
2021-04-13 08:43:41 +0100

    .

--- models/product.js
+++ models/product.js
@@ -1,68 +1,29 @@
-const fs = require("fs")
-const path = require("path")
+// const Cart = require("./cart")
+// const db = require("../util/database")
+const { Sequelize } = require("sequelize")
 
-const Cart = require("./cart")
+const sequelize = require("../util/database")
 
-const p = path.join(
-  path.dirname(require.main.filename),
-  "data",
-  "products.json",
-)
-
-const getProductsFromFile = (cb) => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([])
-    } else {
-      cb(JSON.parse(fileContent))
-    }
-  })
-}
-
-module.exports = class Product {
-  constructor(id, title, description, imageUrl, price) {
-    this.id = id
-    this.title = title
-    this.description = description
-    this.imageUrl = imageUrl
-    this.price = price
-  }
-  save() {
-    getProductsFromFile((products) => {
-      if (this.id) {
-        const existingProductIndex = products.findIndex(
-          (prod) => prod.id === this.id,
-        )
-        const updatedProducts = [...products]
-        updatedProducts[existingProductIndex] = this
-        fs.writeFile(p, JSON.stringify(updatedProducts), (err) =>
-          console.log(err),
-        )
-      } else {
-        this.id = Math.random().toString()
-        products.push(this)
-        fs.writeFile(p, JSON.stringify(products), (err) => console.log(err))
-      }
-    })
-  }
-  static deleteById(id) {
-    getProductsFromFile((products) => {
-      const product = products.find((prod) => prod.id === id)
-      const updatedProduct = products.filter((p) => p.id !== id)
-      fs.writeFile(p, JSON.stringify(updatedProduct), (err) => {
-        if (!err) {
-          Cart.deleteProduct(id, product.price)
-        }
-      })
-    })
-  }
-  static fetchAll(cb) {
-    getProductsFromFile(cb)
-  }
-  static findById(id, cb) {
-    getProductsFromFile((products) => {
-      const product = products.find((p) => p.id === id)
-      cb(product)
-    })
-  }
-}
+const product
+// module.exports = class Product {
+//   constructor(title, description, price, imageUrl, id) {
+//     this.title = title
+//     this.description = description
+//     this.price = price
+//     this.imageUrl = imageUrl
+//     this.id = id
+//   }
+//   save() {
+//     return db.query(
+//       "INSERT INTO products (title, description, price, imageuri) VALUES($1, $2, $3, $4)",
+//       [this.title, this.description, this.price, this.imageUrl],
+//     )
+//   }
+//   static deleteById(id) {}
+//   static fetchAll() {
+//     return db.query("SELECT * FROM products")
+//   }
+//   static findById(id) {
+//     return db.query("SELECT * FROM products WHERE id = $1", [id])
+//   }
+// }
